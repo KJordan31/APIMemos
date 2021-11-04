8b507ddd43f9af11ad951673338dccbd874c0b32
KJordan31 <kjhr31@unitec.edu>
2021-11-04 09:38:45 -0600

    Primera Entidad

---
+++ Dominio/Estado.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Dominio
+{
+    public class Estado
+    {
+        public int Id_Estado { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime SistemaFecha { get; set; }
+        public string SistemaUsuario { get; set; }
+    }
+}
